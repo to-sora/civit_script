180933eb363e93c52d3318ff3a06e813764ade0c
som <60461394+to-sora@users.noreply.github.com>
2026-01-28 19:42:49 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,34 @@
+linker() {
+    local LINK_PATH="$1"
+    local TARGET_PATH="$2"
+    
+    if [ -d "$LINK_PATH" ] && [ ! -L "$LINK_PATH" ]; then
+        echo "WARNING: $LINK_PATH is a directory. Exit."
+        return 1
+    fi
+    
+    if [ -L "$LINK_PATH" ]; then
+        echo "DEBUG: Removing existing symlink: $LINK_PATH"
+        rm "$LINK_PATH"
+        echo "DEBUG: Creating symlink: $LINK_PATH -> $TARGET_PATH"
+        ln -s "$TARGET_PATH" "$LINK_PATH"
+        return 0
+    fi
+    
+    if [ -e "$LINK_PATH" ]; then
+        echo "WARNG:  Existing file: $LINK_PATH EXIT"
+        return 1
+    fi
+    
+    echo "DEBUG: Creating new symlink: $LINK_PATH -> $TARGET_PATH"
+    ln -s "$TARGET_PATH" "$LINK_PATH"
+    return 0
+}
+
+cd "$HOME/ComfyUI/"
+source venv/bin/activate
+linker "$HOME/ComfyUI/input" "/mnt/4T/Comfyui_MAIN/input"
+linker "$HOME/ComfyUI/output" "/mnt/4T/Comfyui_MAIN/output"
+find "$HOME/ComfyUI/models" -type l -exec sh -c 'rm "$1"' sh {} \;
+bash "$HOME/ComfyUI/civit_script/Comfyui_link_builder.sh" models/ /mnt/4T/Comfyui_P2/models/ /mnt/4T/Comfyui_P1/models/ /mnt/4T/Comfyui_MAIN/models/
+python main.py --enable-manager --listen 0.0.0.0
